b0e97cb2af86dadcdc2e20c2f6eed36641dedc60
Berkan Unal <Berkanunal@gmail.com>
2020-05-02 23:05:24 +0300

    Flesh out scrape tournm list job

--- src/jobs/scrape-tournament-list.ts
+++ src/jobs/scrape-tournament-list.ts
@@ -1,39 +1,88 @@
 import axios from "axios";
 import {JSDOM} from "jsdom";
-import {Tournament} from "../entity/tournament";
 
-async function scrapeTheTournamentPage() {
+import {
+  Tournament,
+  TournamentTier,
+} from "../entity/tournament";
+import {parseRawDates} from "../utils/date-utils";
+
+export async function scrapeTheTournamentList() {
   const {data} = await axios.get(
-    "https://liquipedia.net/dota2/Epic_League/Prime/1",
+    "https://liquipedia.net/dota2/Portal:Tournaments",
   );
 
   const {
     window: {document},
   } = new JSDOM(data);
 
-  const tournaments = ["fixme: scrape the list"];
+  const tournamentRows = document.getElementsByClassName(
+    "divRow",
+  );
 
   for (
     let index = 0;
-    index < tournaments.length;
+    index < tournamentRows.length;
     index += 1
   ) {
-    const tournamentName =
-      "fixme: scrape tournament name"; // maybe i dont need this. Since i will come here from tournaments list.
+    const element = tournamentRows[index];
 
-    const tournamentsElement = tournaments[index];
+    const tournamentName = element
+      .getElementsByClassName("divCell Name")[0]
+      .getElementsByClassName(
+        "divCell mobile-hide",
+      )[0]
+      .getElementsByTagName("a")[0]
+      .innerHTML.trim();
 
-    /* const tournamentName = tournamentsElement
-      .getElementsByClassName("tournaments-header")[0]
-      .innerHTML.trim(); */
+    const tier = (element.getElementsByClassName(
+      "divCell Tier",
+    )[0] as HTMLDivElement)
+      .getElementsByTagName("a")[0]
+      .innerHTML.trim()
+      .toLowerCase() as TournamentTier;
 
-    const tournament = new Tournament({
+    const rawDates = (element.getElementsByClassName(
+      "divCell EventDetails Date Header",
+    )[0] as HTMLDivElement).innerHTML.trim();
+
+    const [
+      startDate,
+      endDate
+    ] = parseRawDates(
+      rawDates,
+    );
+
+    const location =
+      (element.getElementsByClassName(
+        "divCell EventDetails Location Header",
+      )[0] as HTMLDivElement)
+        .getElementsByClassName("CountryText")[0]
+        .innerHTML.trim() === "online"
+        ? "online"
+        : "offline";
+
+    const rawPrizePool = (element.getElementsByClassName(
+      "divCell EventDetails Prize Header",
+    )[0] as HTMLDivElement).innerHTML
+      .trim()
+      .replace("$", "").replace(",", "");
+
+    const prizePool = Number.isNaN(
+      parseInt(rawPrizePool, 10),
+    )
+      ? 0
+      : parseInt(rawPrizePool, 10);
+
+    const tournament = new Tournament();
+    tournament.construct({
       name: tournamentName,
-      tier: "major", // FIXME.
-      startDate: new Date(),
-      endDate: new Date(),
+      startDate,
+      endDate,
+      prizePool,
+      location,
+      tier,
     });
+    tournament.save();
   }
 }
-
-scrapeTheTournamentPage();
